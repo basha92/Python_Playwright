0f180a182dfd903dac9cdfcfc15fbd7cce3f3d20
basha92 <sikkandarbasha1992@gmail.com>
2025-09-01 17:04:30 +0530

    Variables concept

---
+++ Variables.py
@@ -0,0 +1,37 @@
+#type casting
+x=str("5")
+y=int(5)
+z=float(5)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#string - can be given in single or double quotes
+
+#case sensitivity
+a=10
+A=20
+print(a)
+print(A)
+#different ways to declare variable
+_ab=3
+a_b=5
+ab5=6
+#2cb=4 -- can not start with no. syntax error
+#multi assignment
+a,c,b = "apple", 'orange', "carrot"
+print(a)
+print(b)
+print(c)
+#single value to multi variables
+x=y=z="orange"
+print(x)
+print(y)
+print(z)
+
+
