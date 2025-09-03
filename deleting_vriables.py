820826a71e39c8804f94e020418c23a965ed7bdb
basha92 <sikkandarbasha1992@gmail.com>
2025-09-03 13:44:06 +0530

    about deleting variables

---
+++ deleting_vriables.py
@@ -0,0 +1,10 @@
+#use del keyword to delete variables
+a = 10
+b = 20
+print(a, b)
+del a   #deleting variable a
+print(b)
+print(a)  #this will raise an error as a is deleted   
+
+#operator - a symbol which will perform operation between two or more vatiables
+#operand - a variable on which operator will perform operation
