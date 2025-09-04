2ec8ea93493968f71bbaae1d56b7d2956726715b
basha92 <sikkandarbasha1992@gmail.com>
2025-09-04 16:51:39 +0530

    About Operators

---
+++ Operators.py
@@ -0,0 +1,30 @@
+#arithmatic operators
+a=10
+b=20
+print("a+b=",a+b) #addition
+print("a-b=",a-b) #subtraction
+print("a*b=",a*b) #multiplication
+print("a/b=",a/b) #division
+print("a//b=",a//b) #floor division
+print("a%b=",a%b) #modulus
+print("a**b=",a**b) #exponentiation
+
+#relatoional operators; always returns boolean value
+x=10
+y=20
+print("x>y=",x>y) #greater than
+print("x<y=",x<y) #less than
+print("x>=y=",x>=y) #greater than or equal to
+print("x<=y=",x<=y) #less than or equal to
+
+#logical operators
+p=True
+q=False
+print("p and q=",p and q) #logical and
+print("p or q=",p or q) #logical or
+print("not p=",not p) #logical not
+print("not q=",not q) #logical not
+print("p and not q=",p and not q) #combination of logical operators
+print("not p or q=",not p or q) #combination of logical operators
+print("p or not q=",p or not q) #combination of logical operators
+print("not p and q=",not p and q) #combination of logical operators
