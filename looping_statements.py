a665f5a9a1bc882249af3300b6b58dfe0b01649f
basha92 <sikkandarbasha1992@gmail.com>
2025-11-26 13:55:17 +0530

    for loop

---
+++ looping_statements.py
@@ -0,0 +1,27 @@
+#range function in python
+#range(10) prints numbers from 0 to 9
+#range(1,10) prints numbers from 1 to 9
+#range(1,10,2) prints numbers from 1 to 9 with a step of 2
+#range(10,1,-1) prints numbers from 10 to 2 in reverse
+#print(list(range(10))) #prints numbers from 0 to 9
+#print(list(range(1, 10))) #prints numbers from 1 to 9
+#print(list(range(1, 10, 2))) #prints numbers from 1 to 9 with a step of 2
+#print(list(range(10, 1, -1))) #prints numbers from 10 to 2 in reverse
+
+#for loop with range function
+#for i in range(1, 11): #prints numbers from 1 to 10
+    #print(i)
+#printing even numbers from 1 to 20
+#for i in range(0, 21, 2):  #prints even numbers from 0 to 20
+    #print(i)
+#printing odd numbers from 1 to 20
+# for i in range(1, 21, 2):  #prints odd numbers from 0 to 20
+    #print(i)
+
+#while loop
+#counting from 1 to 10 using while loop
+i = 1
+while i <= 10: #prints numbers from 1 to 10
+     print(i)
+     i += 1
+print("Done!!")
