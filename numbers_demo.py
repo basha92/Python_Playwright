558b3d2b97012e45bb2a8b4e689d349110f5642d
basha92 <sikkandarbasha1992@gmail.com>
2025-12-03 16:13:50 +0530

    number functions

---
+++ numbers_demo.py
@@ -0,0 +1,12 @@
+num1 = 100
+num2 = 20.5
+print(type(num1))
+print(type(num2))
+
+#find max and min
+print("Maximum value is:", max(10, 20, 30, 40, 50))
+print("Minimum value is:", min(10, 20, 30, 40, 50))
+
+#max and min in float
+print("Maximum value is:", max(10.5, 20.5, 30.5, 40.5, 50.5))
+print("Minimum value is:", min(10.5, 20.5, 30.5, 40.5, 50.5))
