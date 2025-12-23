45660177d14d3a83294c7bf94b6ea1bf537361bb
basha92 <sikkandarbasha1992@gmail.com>
2025-12-23 16:17:36 +0530

    List Basics

---
+++ ListDemo.py
@@ -0,0 +1,21 @@
+# List is collection which is ordered and changeable. Allows duplicate members.it is written in square brackets.Mutable(can change the existing values)
+#creating a list
+my_list = [1, 2, 3, 4, 5]
+my_list2 = ["apple", "banana", "cherry"]
+my_list3 = [1, "hello", 3.14, True]
+my_list4 = []
+#print("Original Lists:")
+#print(my_list)
+#print(my_list2)
+#print(my_list3)
+#print(my_list4)
+
+# Accessing elements
+print("Accessing elements:")
+print(my_list[0])        # First element
+print(my_list2[1])      # Second element
+print(my_list3[-1])     # Last element
+print(my_list[1:4])     # Slicing
+print(my_list2[:2])     # Slicing from start
+print(my_list3[2:])     # Slicing to end
+
