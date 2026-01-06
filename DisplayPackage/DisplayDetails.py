41d7d52df6f9ea41fad8562fad2377b468780ca4
basha92 <sikkandarbasha1992@gmail.com>
2026-01-06 16:02:43 +0530

    try with main

--- DisplayPackage/DisplayDetails.py
+++ DisplayPackage/DisplayDetails.py
@@ -3,16 +3,20 @@
 #2. EMI calculation from LoanClass module inside ModulesAndPackages package
 #3. employee details from EmployeeDetails module inside EmployeePackage
 
-from ModulesAndPackages.StudentPackage.StudentDetails import get_student_details
 
-student_id = 3
-student_info = get_student_details(student_id)
-print(f"Details of student with ID {student_id}: {student_info}")
+import sys
+sys.path.append('F:\\Python_Git\\Python_Playwright\\ModulesAndPackages')
 
-from ModulesAndPackages.LoanClass import EMICalculator
-home_loan = EMICalculator(500000, 7.5, 20)
-print("Home Loan EMI:", f"{home_loan.calculate_emi():.2f}")
+#from ModulesAndPackages.StudentPackage.StudentDetails import get_student_details
 
-from EmployeePackage.EmployeeDetails import Employee
-emp1 = Employee('John', 'Doe', 60000)
-print(f"Employee: {emp1.fullname()}, Email: {emp1.email}, Pay: {emp1.pay}")
+#student_id = 3
+#student_info = get_student_details(student_id)
+#print(f"Details of student with ID {student_id}: {student_info}")
+
+#from ModulesAndPackages.LoanClass import EMICalculator
+#home_loan = EMICalculator(500000, 7.5, 20)
+#print("Home Loan EMI:", f"{home_loan.calculate_emi():.2f}")
+
+#from EmployeePackage.EmployeeDetails import Employee
+#emp1 = Employee('John', 'Doe', 60000)
+#print(f"Employee: {emp1.fullname()}, Email: {emp1.email}, Pay: {emp1.pay}")
