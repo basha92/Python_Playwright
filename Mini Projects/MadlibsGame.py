36a98e3113c963a53800f736a38c3f5213714a3b
basha92 <sikkandarbasha1992@gmail.com>
2026-01-09 21:53:17 +0530

    Word Game

---
+++ Mini Projects/MadlibsGame.py
@@ -0,0 +1,12 @@
+#Madlibs game is a word game where you create a stroy by filling the blanks with random words.
+#taking inputs from user
+adjective1 = input("Enter an adjective(description):")
+place = input("Enter a place:")
+noun = input("Enter noun(person, place, thing):")
+adjective2 = input("Enter an adjective (description):")
+food = input("Enter food:")
+
+#story
+print(f"One day, my {adjective1} boyfriend decided to go on an adventure to the {place}.")
+print(f"He packed his favorite {noun} and a {adjective2} snack because, of course, ")
+print(f"you can’t adventure without {food}!")
