7b063250234a3f105ce2333a02227d778a0972fe
basha92 <sikkandarbasha1992@gmail.com>
2026-01-10 02:04:39 +0530

    Calculator using if else

---
+++ Mini Projects/Basic_Calculator.py
@@ -0,0 +1,23 @@
+#basic calculator using if else
+
+operator = input ("Select an operator (+ - * /):")
+operatorlist = ['+', '-', '*', '/']
+if operator not in operatorlist:
+    print("Invalid Input")
+else:
+    num1 = float(input("Enter 1st number:"))
+    num2 = float(input("Enter 2nd number:"))
+if operator == "+":
+    result = num1+num2
+    print(round(result),3)
+elif operator == "-":
+    result = num1-num2
+    print(round(result),3)
+elif operator == "*":
+    result = num1*num2
+    print(round(result),3)
+elif operator == "/":
+    result = num1/num2
+    print(round(result),3)
+
+
