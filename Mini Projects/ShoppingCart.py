8cd5a4940bb011a0b7c4754db36f59aff15c61b4
basha92 <sikkandarbasha1992@gmail.com>
2026-01-10 22:39:39 +0530

    Lists practice

--- Mini Projects/ShoppingCart.py
+++ Mini Projects/ShoppingCart.py
@@ -1,1 +1,22 @@
 #using while loop and lists
+
+#res = "\n".join("{} {}".format(x, y) for x, y in zip(a, b)) - joining 2 lists
+
+foods=[]
+prices=[]
+total=0
+
+while True:
+    food = input("Enter the food (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of {food}: $"))
+        foods.append(food)
+        prices.append(price)
+        result = "\n".join("{}: $ {}".format(x, y) for x, y in zip(foods, prices))
+        total = total+price
+
+print("------YOUR CART-----")
+print(result)
+print("Total: ", total)
