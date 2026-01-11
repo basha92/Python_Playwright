8ed3623da00629326b5b921ba5c7b71e7e026b13
basha92 <sikkandarbasha1992@gmail.com>
2026-01-12 01:26:24 +0530

    using list and tuples

---
+++ Mini Projects/QuizGame.py
@@ -0,0 +1,38 @@
+#quiz game using list and tuples
+
+questions = ("What company makes the Xperia line of smartphones?", 
+             "Which city is home to the Brandenburg Gate?", 
+             "What is the fourth state of matter, after solid, liquid, and gas?", 
+             "What are plants that lose their leaves in autumn called?", 
+             "Who is generally considered the inventor of the motor car?")
+options = (("A.Samsung", "B.Sony", "C.Nokia", "D.Apple"), 
+           ("A.Paris", "B.Berlin", "C.Rome", "D.London"),
+           ("A.Ether", "B.Plasma ", "C.Jelly ", "D.Earth "), 
+           ("A.Coniferous", "B.Evergreen", "C.Carnivorous", "D.Deciduous"), 
+           ("A.Henry Ford ", "B.Karl Benz", "C.Gottlieb Daimler", "D.Nikola Tesla"))
+answers = ("B", "B", "C", "D", "B")
+guesses=[]  #appending user guess. so list.
+score = 0
+question_number = 0
+
+#displaying the questions with options
+for question in questions:
+    print("------------------------------")
+    print(question)
+    for option in options[question_number]:
+        print(option)
+    
+    #taking user input for answer
+    guess = input("Enter the option (A, B, C, D): ").upper()
+    guesses.append(guess)
+    #scoring
+    if guess == answers[question_number]:
+        score = score+1
+        print("CORRECT!")
+    else:
+        print("INCORRECT")
+        print(f"{answers[question_number]} is the correct answer")
+    question_number += 1
+print("------------------------------")
+print(f"Yor score is {score}")
+print("------------------------------")
