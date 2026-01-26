57cf2a9a0fa9e23bb837eaf9a4731eefb2566a84
basha92 <sikkandarbasha1992@gmail.com>
2026-01-26 23:18:58 +0530

    using pytest

--- PlayWrightConcepts/sync.py
+++ PlayWrightConcepts/sync.py
@@ -16,13 +16,13 @@ import re  #import regular expressions
 from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://playwright.dev/", headless=False)
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
 
 def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("https://playwright.dev/", headless=False)
 
     # Click the get started link.
     page.get_by_role("link", name="Get started").click()
